73963a1fe589d5d7747c6f968f5b392b336f6d73
Elijah Brady <bradyeli09@gmail.com>
2023-08-24 12:50:59 -0500

    Prayin this works lol

--- Assignment-0.c
+++ Assignment-0.c
@@ -94,19 +94,24 @@ int main(int argc, char *argv[]) {
     int8_t out[1024][1024];
     int8_t out_x[1024][1024];
     int8_t out_y[1024][1024];
+    int8_t K_x[3][3] = {
+            {-1, 0, 1},
+            {-2, 0, 2},
+            {-1, 0, 1}
+    };
+    int accumulator_x;
+    int8_t K_y[3][3] = {
+            {-1, -2, -1},
+            {0,  0,  0},
+            {1,  2,  1}
+    };
+    int accumulator_y;
 
     /* Example usage of PGM functions */
     /* This assumes that motorcycle.pgm is a pgm image of size 1024x1024 */
     /* This method seems to copy what is in motorcycle.pgm and put it into image */
     read_pgm("motorcycle.pgm", image, 1024, 1024);
 
-    int8_t K_x[3][3] = {
-            {-1, 0, 1},
-            {-2, 0, 2},
-            {-1, 0, 1}
-    };
-
-    int accumulator_x;
     for (int r = 0; r < 1024; r++) {
         for (int c = 0; c < 1024; c++) {
             accumulator_x = 0;
@@ -116,53 +121,28 @@ int main(int argc, char *argv[]) {
                                     K_x[j][i] * (int) (image[r + (j - (int) ceil(3 / 2))][c + (int) (i - ceil(3 / 2))]) * 2;
                 }
             }
+            // Figure this out
             out_x[r][c] = abs(accumulator_x) > 255 ? 255 : accumulator_x;
         }
     }
 
-    int8_t K_y[3][3] = {
-            {-1, -2, -1},
-            {0,  0,  0},
-            {1,  2,  1}
-    };
-    int accumulator_y;
     for (int r = 0; r < 1024; r++) {
         for (int c = 0; c < 1024; c++) {
             accumulator_y = 0;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     accumulator_y = accumulator_y +
-                                    K_y[j][i] * image[r + (j - (int) ceil(3 / 2))][c + (int) (i - ceil(3 / 2))];
+                                    K_y[j][i] * (int) (image[r + (j - (int) ceil(3 / 2))][c + (int) (i - ceil(3 / 2))]) * 2;
                 }
             }
-            out_y[r][c] = accumulator_y;
+            out_y[r][c] = abs(accumulator_y) > 255 ? 255 : accumulator_y;
         }
     }
 
     // Matrix multiplication for x and y matrices
-    int8_t out_x2[1024][1024];
     for (int i = 0; i < 1024; i++) {
         for (int j = 0; j < 1024; j++) {
-            out_x2[i][j] = 0;
-            for (int k = 0; k < 1024; k++) {
-                out_x2[i][j] += out_x[i][k] * out_x[k][j];
-            }
-        }
-    }
-
-    int8_t out_y2[1024][1024];
-    for (int i = 0; i < 1024; i++) {
-        for (int j = 0; j < 1024; j++) {
-            out_y2[i][j] = 0;
-            for (int k = 0; k < 1024; k++) {
-                out_y2[i][j] += out_y[i][k] * out_y[k][j];
-            }
-        }
-    }
-
-    for (int i = 0; i < 1024; i++) {
-        for (int j = 0; j < 1024; j++) {
-            out[i][j] = (int8_t) sqrt(out_x2[i][j] + out_y2[i][j]);
+            out[i][j] = (int8_t) sqrt((out_x[i][j] * out_x[i][j]) + (out_y[i][j] * out_y[i][j]));
         }
     }
 
